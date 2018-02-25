c75a9220cfbbd1be042baae1d758c078399be776
Olav Ljosland <olavlj@me.com>
2018-02-25 16:24:08 +0100

    Play video at spesific time

---
+++ main.py
@@ -0,0 +1,34 @@
+import os, sys, time
+from random import choice
+
+
+def set_alarm(alarm_time):
+    ctime = time.strftime("%H:%M")
+    print(ctime)
+
+    while ctime != alarm_time:
+        ctime = time.strftime("%H:%M")
+        print("{} - {}".format(ctime, alarm_time))
+        time.sleep(1)
+
+    print("Play episode")
+    run_alarm()
+
+
+def run_alarm():
+    all_episodes = os.listdir("episodes")
+
+    chosen_episode = choice(all_episodes)
+
+    os.system("omxplayer {}".format(chosen_episode))
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    set_alarm(argv)
+
+
+if __name__ == "__main__":
+    main("16:09")
